228f1f56c63ac7a8a3230dc20492b6e0ec43f32a
mdferdausalam19 <mdferdausalam333@gmail.com>
2024-10-09 20:05:02 +0600

    Add navbar and implement light/dark theme toggle functionality

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <h2>This is a home page.</h2>
+    </div>
+  );
+};
+
+export default Home;
